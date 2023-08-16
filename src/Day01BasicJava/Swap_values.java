81200730e8e791d3fa15748396442170780ecf6b
Prathamesh-Narayane <prathameshnarayane2802@gmail.com>
2023-08-16 23:06:51 +0530

    Day1

--- src/Day01BasicJava/Swap_values.java
+++ src/Day01BasicJava/Swap_values.java
@@ -8,7 +8,7 @@ public class Swap_values {
         System.out.println("enter A and B value");
         int A = sc.nextInt();
         int B = sc.nextInt();
-        System.out.println("A=" + A + " ||  B="+ B);
+        System.out.println("A=" + A + "  ||  B="+ B);
         A=A+B;
         B=A-B;
         A=A-B;
